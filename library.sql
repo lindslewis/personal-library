db9cae699b03da248bf3b630dcd2f6ab8d3979d5
LindsayLew <lmschwehr@gmail.com>
2022-07-26 09:50:16 -0700

    added a novels table

--- library.sql
+++ library.sql
@@ -1,3 +1,11 @@
-DROP DATABASE IF EXISTS library
-CREATE DATABASE library
+DROP DATABASE IF EXISTS library;
+CREATE DATABASE library;
 
+USE library;
+
+CREATE TABLE novels (
+    id INT NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    year INT NOT NULL,
+    author VARCHAR (100) NOT NULL
+);
---
+++ novels.sql
@@ -0,0 +1,11 @@
+INSERT INTO novels (id, name, year, author)
+
+VALUES 
+    (001, "A Court of Thorns and Roses", 2015, "Sarah J. Maas"),
+    (002, "A Court of Mist and Fury", 2016, "Sarah J. Maas"),
+    (003, "A Court of Wings and Ruin", 2017, "Sarah J. Maas"),
+    (004, "Five Feet Apart", 2018, "Rachael Lippincott"),
+    (005, "Everything, Everything", 2015, "Nicola Yoon"),
+    (006, "Mexican Gothic", 2020, "Silvia Moreno-Garcia");
+
+SELECT * FROM novels;

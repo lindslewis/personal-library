e9fda48c1b036cc16f7aa80cc55b3fdf22428088
LindsayLew <lmschwehr@gmail.com>
2023-01-18 16:25:11 -0800

    added more into the manga table

--- manga.sql
+++ manga.sql
@@ -2,7 +2,13 @@
 INSERT INTO manga (id, name, volumes, mangaka, complete)
 
 VALUES
-    (001, "Ao Haru Ride", "1-4", "Io Sakisaka", false),
-    (002, "Daytime Shooting Star", "1-4", "Mika Yamamori", false);
+    (011, "Dengeki Daisy", "1-8", "Kyousuke Motomi", false),
+    (012, "My Hero Academia", "1-8", "Kohei Horikoshi", false),
+    (013, "Maximum Ride", "1-4", "James Patterson", false),
+    (014, "Hana Kimi", "1-14", "Hisaya Nakajo", false),
+    (015, "Full Moon", "1", "Arina Tanemura", false),
+    (016, "Stepping on Roses", "1-2", "Rinko Ueda", false),
+    (017, "Skip Beat", "1-37", "Yoshiki Nakamura", false),
+    (018, "Boys Over Flowers", "35", "Yoko Kamio", false);
 
 SELECT * FROM manga;

e82dbd7142d3de3e7e59c815d501c89cd1f8e9e1
LindsayLew <lmschwehr@gmail.com>
2023-01-18 16:07:44 -0800

    figured out syntax issue in manga sql file

--- manga.sql
+++ manga.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO manga (id, name, volumes, mangaka, complete);
+INSERT INTO manga (id, name, volumes, mangaka, complete)
 
 VALUES
-    (001, "Ao Haru Ride", "1-4", "Io Sakisaka", false),
-    (002, "Daytime Shooting Star", "1-4", "Mika Yamamori", false);
+    (001, "Ao Haru Ride", "1, 2, 3, 4", "Io Sakisaka", false),
+    (002, "Daytime Shooting Star", "1, 2, 3, 4", "Mika Yamamori", false);
 
 SELECT * FROM manga;

34a3791983c4de0cc3e922eadf19eb5f5c6c2c08
LindsayLew <lmschwehr@gmail.com>
2023-01-18 16:17:44 -0800

    replaced id 1 and 2

--- manga.sql
+++ manga.sql
@@ -2,7 +2,7 @@
 INSERT INTO manga (id, name, volumes, mangaka, complete)
 
 VALUES
-    (001, "Ao Haru Ride", "1, 2, 3, 4", "Io Sakisaka", false),
-    (002, "Daytime Shooting Star", "1, 2, 3, 4", "Mika Yamamori", false);
+    (001, "Ao Haru Ride", "1-4", "Io Sakisaka", false),
+    (002, "Daytime Shooting Star", "1-4", "Mika Yamamori", false);
 
 SELECT * FROM manga;
